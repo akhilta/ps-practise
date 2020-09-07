dcc6e92cd74147738e7eef24ca229316ff81a147
Akhil Tamatam <akhilreddy.tamatam@gmail.com>
2020-09-07 09:53:09 +0530

    devloper2 commit

--- java1.java
+++ java1.java
@@ -2,7 +2,8 @@ class java1 {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
 	System.out.println("Hello, World! from developer branch"); 
-	System.out.println("Hello, World! from developer2 branch"); 
+	System.out.println("Hello, World! from developer2 branch   "); 
+	
 
     }
 }
